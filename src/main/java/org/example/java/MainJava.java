7e29cf9e80381c79cfc4134fe73997ddba2d1b6b
linda <charming.linda85@gmail.com>
2024-12-12 21:51:42 +0300

    Test is done

--- src/main/java/org/example/java/MainJava.java
+++ src/main/java/org/example/java/MainJava.java
@@ -1,7 +1,26 @@
 package org.example.java;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainJava {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = {1, 3, 4, 5, 1, 5, 4};
+        Map<Integer, Integer> result = countElements(array);
+
+        System.out.println(result);
+    }
+
+    public static Map<Integer, Integer> countElements(int[] incomingArray) {
+        Map<Integer, Integer> elementsCountHashMap = new HashMap<>();
+
+        for (int element : incomingArray) {
+            if (elementsCountHashMap.containsKey(element)) {
+                elementsCountHashMap.put(element, elementsCountHashMap.get(element) + 1);
+            } else {
+                elementsCountHashMap.put(element, 1);
+            }
+        }
+        return elementsCountHashMap;
     }
 }
